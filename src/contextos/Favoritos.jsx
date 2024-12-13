3cbc97af9819128627b63032a98b4d000a9d2657
Felipe Sousa Chagas <felypegt8@gmail.com>
2024-12-13 18:28:17 -0300

    adicionar hook useFavorito

--- src/contextos/Favoritos.jsx
+++ src/contextos/Favoritos.jsx
@@ -1,11 +1,11 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const FavoritosContext = createContext;
 FavoritosContext.displayName = "Favoritos";
 
-export default function FavoritosProvider({children}){
+export default function FavoritosProvider({ children }) {
     const [favorito, setFavorito] = useState([]);
-    return(
+    return (
         <FavoritosProvider
             value={{
                 favorito,
@@ -15,4 +15,28 @@ export default function FavoritosProvider({children}){
             {children}
         </FavoritosProvider>
     )
+}
+
+export function useFavoritoContext() {
+    const { favorito, setFavorito } = useContext(FavoritosContext);
+
+    function adicionarFavorito(novoFavorito) {
+        const favoritoRepetido = favorito.some(item => item.id === novoFavorito.id)
+
+        let novaLista = [...favorito];
+
+        if (!favoritoRepetido) {
+            novaLista.push(novoFavorito);
+            setFavorito(novaLista);
+        }
+
+        novaLista.splice(novaLista.indexOf(novoFavorito), 1);
+
+        return setFavorito(novaLista);
+    }
+
+    return {
+        favorito,
+        adicionarFavorito,
+    }
 }
